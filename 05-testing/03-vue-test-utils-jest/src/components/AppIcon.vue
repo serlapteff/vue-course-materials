28f28c347f25084c6b255e72116ead3eba884598
Grigorii K. Shartsev <me@shgk.me>
2020-12-28 20:21:08 +0500

    Add 05-testing start

---
+++ 05-testing/03-vue-test-utils-jest/src/components/AppIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <img class="icon" alt="icon" :src="iconSrc" />
+</template>
+
+<script>
+export default {
+  name: 'AppIcon',
+
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    iconSrc() {
+      return require(`@/assets/icons/icon-${this.icon}.svg`);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
